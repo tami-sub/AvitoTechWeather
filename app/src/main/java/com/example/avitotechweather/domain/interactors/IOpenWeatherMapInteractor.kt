2f1ed93d5236d3a37a6735d19e534da9809bad52
taagi mikailov <ment2010_47@mail.ru>
2022-11-12 04:16:03 +0300

    fixed di

--- app/src/main/java/com/example/avitotechweather/domain/interactors/IOpenWeatherMapInteractor.kt
+++
@@ -1,8 +0,0 @@
-package com.example.avitotechweather.domain.interactors
-
-import com.example.avitotechweather.domain.entity.CityDTO
-import retrofit2.Call
-
-//interface IOpenWeatherMapInteractor {
-//    suspend fun getCityLatLon(cityName: String) : Call<CityDTO>
-//}
